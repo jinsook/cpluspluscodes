e2f0947401c06ad48e6d816752a96c948c0178fa
Jin <choijinsook@gmail.com>
2014-09-19 15:18:32 -0700

    study array and strings

--- array_strings.h
+++ array_strings.h
@@ -5,4 +5,12 @@ using namespace std;
 bool hasAllUniqueChars(string str);
 bool hasAllUniqueChars2(string str);
 bool hasAllUniqueChars3(string str);
+
+void reverseString(char* str);
+void reverseString2(char* str);
+
+bool isPermutation(const string& str1, const string& str2);
+bool isPermutation2(const string& str1, const string& str2);
+
+
 #endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <boost/array.hpp>
+#include <string.h>
 
 #include "array_strings.h"
 
@@ -29,5 +30,34 @@ int main(int argc , char**argv)
 
     cout<<hasAllUniqueChars3( string("abcdeefgh"))<<endl;
     cout<<hasAllUniqueChars3( string("abcdefghi"))<<endl;
+
+    char str1[256];
+
+    strcpy( str1, "jin sook");
+    cout<<str1<<endl;
+    reverseString( str1);
+    cout<<str1<<endl;
+
+    strcpy( str1, "jin");
+    cout<<str1<<endl;
+    reverseString( str1);
+    cout<<str1<<endl;
+
+    strcpy( str1, "jin sook");
+    cout<<str1<<endl;
+    reverseString2( str1);
+    cout<<str1<<endl;
+
+    strcpy( str1, "jin");
+    cout<<str1<<endl;
+    reverseString2( str1);
+    cout<<str1<<endl;
+
+    cout<<"isPermutation:abca:cbaa-->"<<isPermutation(string("abca"), string("cbaa") )<<endl;
+    cout<<"isPermutation:abc:baa-->"<<isPermutation(string("abc"), string("baa"))<<endl;
+
+    cout<<"isPermutation2:abca:cbaa-->"<<isPermutation2(string("abca"), string("cbaa") )<<endl;
+    cout<<"isPermutation2:abc:baa-->"<<isPermutation2(string("abc"), string("baa"))<<endl;
+
     return 0;
 }
